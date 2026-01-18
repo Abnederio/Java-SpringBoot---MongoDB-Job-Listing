ae201dff8a0bf70b4f574c4919598ab192a52e71
Andrei Turgo <abturgo@gmail.com>
2026-01-18 21:43:55 +0800

    Updated AOP Logging.

--- src/main/java/com/turgo/joblisting/Aspect/LoggingAspect.java
+++ src/main/java/com/turgo/joblisting/Aspect/LoggingAspect.java
@@ -1,13 +1,10 @@
 package com.turgo.joblisting.Aspect;
 //AOP
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import com.turgo.joblisting.Model.Post;
-
-import java.util.logging.Logger;
 
 
 @Aspect
@@ -16,14 +13,19 @@ public class LoggingAspect {
 
     private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Before("execution(* com.turgo.joblisting.Controller.PostController.getAllPosts())")
+    @Before("execution(* com.turgo.joblisting.Controller.PostController.getAllPosts())") //Before executiion
     public void logBefore(){
         LOGGER.info("getAll posts called from aspect");
     }
 
-    @After("execution(* com.turgo.joblisting.Controller.PostController.getAllPosts())")
+    @AfterReturning("execution(* com.turgo.joblisting.Controller.PostController.getAllPosts())") //After method execution
     public void logAfter(){
         LOGGER.info("getAll posts method Executed");
+    }
+
+    @AfterThrowing("execution(* com.turgo.joblisting.Controller.PostController.getAllPosts())") //When triggers Exception
+    public void logException(){
+        LOGGER.info("Issue");
     }
 
 }
